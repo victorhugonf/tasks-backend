54335f221c45c1dc41c5b98a1ea90e572f0abc7b
Victor Hugo Soares Cardoso <victorhugo_nf@hotmail.com>
2021-02-25 00:17:27 -0300

    post actions

--- src/test/java/br/ce/wcaquino/taskbackend/utils/DateUtilsTest.java
+++ src/test/java/br/ce/wcaquino/taskbackend/utils/DateUtilsTest.java
@@ -10,7 +10,7 @@ public class DateUtilsTest {
 	@Test
 	public void deveRetornarTrueParaDatasFuturas() {
 		LocalDate date = LocalDate.now().plusDays(1);
-		Assert.assertFalse(DateUtils.isEqualOrFutureDate(date));
+		Assert.assertTrue(DateUtils.isEqualOrFutureDate(date));
 
 		date = LocalDate.now().plusMonths(1);
 		Assert.assertTrue(DateUtils.isEqualOrFutureDate(date));
